da6ef10c7d5a2740688c46a669d82c82c7706d62
apr-16 <anandpr3112@gmail.com>
2022-10-26 15:45:15 +0530

    git init

---
+++ area.py
@@ -0,0 +1,8 @@
+print("enter the values of length and bradth")
+a=int(input())
+b=int(input())
+area=a*b
+peri=2*(a+b)
+print("area=", area)
+print("perimeter=", peri)
+
---
+++ area_of_triangle.py
@@ -0,0 +1,24 @@
+#print("Enter the value for Base")
+#a=int(input())
+#print("Enter the value  for Height")
+#b=int(input())
+#c=a*b
+#area=c/2
+#print("The area is",area)
+
+#import math
+#print("enter the 3 sides of the triangle")
+#a=int(input())
+#b=int(input())
+#c=int(input())
+#s=(a+b+c)/2
+#d=s*((s-a)*(s-b)*(s-c))
+#area=math.sqrt(d)
+#print("Area is ",area)
+import math
+print("enter the side of triangle")
+a=int(input())
+b=math.sqrt(3)
+c=b/4
+area=c*a*a
+print("area is",area)
